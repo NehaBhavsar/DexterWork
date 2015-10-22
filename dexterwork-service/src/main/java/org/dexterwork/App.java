7157db8f7b952f3b9428c7b51884ade24c52624e
neha <nehabhavsar.er@gmail.com>
2015-10-22 09:34:26 -0700

    First commit

---
+++ dexterwork-service/src/main/java/org/dexterwork/App.java
@@ -0,0 +1,21 @@
+package org.dexterwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+
+/**
+ * Hello world!
+ *
+ */
+
+@EnableAutoConfiguration
+@ComponentScan("org.dexterwork")
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+    }
+}
---
+++ dexterwork-service/src/test/java/org/dexterwork/controller/UserController.java
@@ -0,0 +1,5 @@
+package org.dexterwork.controller;
+
+public class UserController {
+
+}
